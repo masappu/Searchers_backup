233d263d084ee6f474d00aca528f2f8b9664c53c
masappu <njzzmmi6tn@icloud.com>
2022-01-09 18:34:55 +0900

    グルメ検索への遷移を実装

---
+++ Seachers/ViewController/GourmandSearchViewController/GourmandSearchViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GourmandSearchViewController.swift
+//  Seachers
+//
+//  Created by 山口誠士 on 2022/01/09.
+//
+
+import UIKit
+
+class GourmandSearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
