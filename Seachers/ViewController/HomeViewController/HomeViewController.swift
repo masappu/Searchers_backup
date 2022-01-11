6be8ee8b0aea278b44929b1c8783d91a6767c0b6
DaigoKondo <daigo.soccer.0106@icloud.com>
2022-01-11 20:07:00 +0900

    storyboard変更

--- Seachers/ViewController/HomeViewController/HomeViewController.swift
+++ Seachers/ViewController/HomeViewController/HomeViewController.swift
@@ -14,39 +14,6 @@ class HomeViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func goNetShoppingVC(_ sender: Any) {
-        let storyboard = UIStoryboard(name: "NetShopping", bundle:nil)
-        let navigationController = storyboard.instantiateInitialViewController() as! UINavigationController
-        let NetShopVC = navigationController.topViewController as! NetShoppingViewController
-        self.navigationController?.pushViewController(NetShopVC, animated: true)
-        
-        
-    }
-    
-    @IBAction func goGourmadSearchVC(_ sender: Any) {
-        let storyboard = UIStoryboard(name: "GourmandSearch", bundle: nil)
-        let navigationController = storyboard.instantiateInitialViewController() as! UINavigationController
-        let gourmandSearchVC = navigationController.topViewController as! GourmandSearchViewController
-        self.navigationController?.pushViewController(gourmandSearchVC, animated: true)
-
-    }
-    
-    
-    @IBAction func goTravelSearchVC(_ sender: Any) {
-        let storyboard = UIStoryboard(name: "TravelSearch", bundle: nil)
-        let navigationController = storyboard.instantiateInitialViewController() as! UINavigationController
-        let travelSearchVC = navigationController.topViewController as! TravelSearchViewController
-        self.navigationController?.pushViewController(travelSearchVC, animated: true)
-
-    }
-    
-    
-    @IBAction func goFavView(_ sender: Any) {
-        let storyboard = UIStoryboard(name: "FavView", bundle: nil)
-        let tabBarController = storyboard.instantiateInitialViewController() as! UITabBarController
-        tabBarController.selectedIndex = 0
-        self.navigationController?.pushViewController(tabBarController, animated: true)
-    }
     
 }
 
