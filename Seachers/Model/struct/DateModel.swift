4c7ef889bbbf5dc845f05a948b9415b5afaaeb1f
masappu <njzzmmi6tn@icloud.com>
2022-01-13 00:27:08 +0900

    GourmandSearchDataModelの実装

--- Seachers/Model/struct/DateModel.swift
+++ Seachers/Model/struct/DateModel.swift
@@ -18,22 +18,27 @@ struct DateModel{
     
     private var dateFormatter: DateFormatter{
         let formatter = DateFormatter()
-        formatter.dateFormat = DateFormatter.dateFormat(fromTemplate: "yyyy/MM/dd HH:mm", options: 0, locale: Locale(identifier: "ja_JP"))
-        
+//        formatter.dateFormat = DateFormatter.dateFormat(fromTemplate: "yyyy年MM月dd日 HH時mm分", options: 0, locale: Locale(identifier: "ja_JP"))
+        formatter.dateFormat = "MM月dd日(EEE) HH時mm分"
+        formatter.locale = Locale(identifier: "ja_JP") 
         return formatter
     }
     
     private func initialDate() -> Date{
         let formatter = DateFormatter()
-        formatter.dateFormat = DateFormatter.dateFormat(fromTemplate: "yyyy/MM/dd", options: 0, locale: Locale(identifier: "ja_JP"))
-        let initialDateString = formatter.string(from: Date()) + "19:00"
+//        formatter.dateFormat = DateFormatter.dateFormat(fromTemplate: "yyyy年MM月dd日", options: 0, locale: Locale(identifier: "ja_JP"))
+        formatter.dateFormat = "MM月dd日(EEE)"
+        formatter.locale = Locale(identifier: "ja_JP")
+        print(formatter.string(from: Date()))
+        let initialDateString = formatter.string(from: Date()) + " 19時00分"
 
-        return formatter.date(from: initialDateString)!
+        return self.dateFormatter.date(from: initialDateString)!
     }
 
     init(){
         self.dateString = String()
         self.date = Date()
         self.date = initialDate()
+        self.dateString = self.dateFormatter.string(from: self.date)
     }
 }
