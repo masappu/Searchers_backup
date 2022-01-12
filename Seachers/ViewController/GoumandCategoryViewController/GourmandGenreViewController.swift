2635411244fb0e78e6b0c16e9a81043628d3a30f
masappu <njzzmmi6tn@icloud.com>
2022-01-13 00:24:53 +0900

    ファイル名の変更

---
+++ Seachers/ViewController/GoumandCategoryViewController/GourmandGenreViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GourmandCategoryViewController.swift
+//  Seachers
+//
+//  Created by 山口誠士 on 2022/01/10.
+//
+
+import UIKit
+
+class GourmandGenreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
